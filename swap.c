99b3d34771dd4fb27e48b1b13478f404d28b7608
Namrita365 <22kt1a0576@gmail.com>
2025-06-24 09:54:55 +0530

    added code for postive and negative numbers using if-else stement

---
+++ swap.c
@@ -0,0 +1,16 @@
+/*pos or neg using if-else*/
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a num:");
+    scanf("%d",&a);
+    if (a>=0)
+    {
+        printf("pos num");
+    }
+    else{
+        printf("neg num");
+    }
+    return 0;
+
+}
