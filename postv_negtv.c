77371e2ad940b6cf8d7ebbb4a74e5acdd687d567
Namrita365 <22kt1a0576@gmail.com>
2025-06-24 10:02:49 +0530

    pos and neg using if-elseif-else

--- postv_negtv.c
+++ postv_negtv.c
@@ -1,1 +1,18 @@
+/*pos neg using if-elseif-else*/
 #include<stdio.h>
+void main(){
+    int a ;
+    printf("enter a:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("pos num");
+
+    }
+    else if(a==0){
+        printf("num is zero");
+    }
+    else{
+        printf("neg num");
+    }
+}
