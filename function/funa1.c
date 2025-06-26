44d6dbca0b83049ef2be7fe3633ea0a6c36ff4e5
Namrita365 <22kt1a0576@gmail.com>
2025-06-26 09:46:07 +0530

    ex-2 for no arg no return val

---
+++ function/funa1.c
@@ -0,0 +1,14 @@
+//Ex:1-prg for no arg no return val
+#include<stdio.h>
+void number()
+{
+    int num;
+    printf("enter anum:");
+    scanf("%d",&num);
+    printf("you have entered :%d",num);
+}
+int main()
+{
+    number();
+    return 0;
+}
