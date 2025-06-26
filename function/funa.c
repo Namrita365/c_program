f0cf3261b63837646f226e0cdb7cd2dd2823a8c4
Namrita365 <22kt1a0576@gmail.com>
2025-06-26 09:40:33 +0530

    no arg no return val

---
+++ function/funa.c
@@ -0,0 +1,18 @@
+/*functions
+1predefined 
+2.user defined
+a.arg pass return val
+b.arg pass no return val
+c.no arg return val 
+d.no arg no return
+*/
+//example for no arg no return value
+#include<stdio.h>
+void hi(){
+    printf("hello gm");
+}
+int main()
+{
+    hi();
+    return 0;
+}
